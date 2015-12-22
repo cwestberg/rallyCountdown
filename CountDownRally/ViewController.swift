d0f6f9fadcdfeaba4871b27f56074716e3603a31
Clarence Westberg <clarence.westberg@gmail.com>
2015-12-21 21:00:52 -0600

    enter time, half woking

--- CountDownRally/ViewController.swift
+++ CountDownRally/ViewController.swift
@@ -25,7 +25,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         self.timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self,
             selector: "updateTimeLabel", userInfo: nil, repeats: true)
     }
@@ -35,13 +34,85 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addTimeBtn(sender: AnyObject) {
+        let currentDate = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponents = calendar.components([NSCalendarUnit.Day, NSCalendarUnit.Month, NSCalendarUnit.Year, NSCalendarUnit.WeekOfYear, NSCalendarUnit.Hour, NSCalendarUnit.Minute, NSCalendarUnit.Second, NSCalendarUnit.Nanosecond], fromDate: currentDate)
+        
+        //Create the AlertController
+        let actionSheetController: UIAlertController = UIAlertController(title: "Alert", message: "Due Time", preferredStyle: .Alert)
+        
+        //Create and add the Cancel action
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+            //Do some stuff
+            
+        }
+        actionSheetController.addAction(cancelAction)
+        
+        //Create and an start action
+        let startAction: UIAlertAction = UIAlertAction(title: "Add", style: .Default) { action -> Void in
+            //Do some other stuff
+            let date = NSDate()
+            let formatter = NSDateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss ZZZ";
+            let defaultTimeZoneStr = formatter.stringFromDate(date)
+            formatter.timeZone = NSTimeZone(abbreviation: "-600");
+
+            print("defaultTimeZoneStr \(defaultTimeZoneStr)")
+
+            let hours = actionSheetController.textFields![0]
+            let minutes = actionSheetController.textFields![1]
+            let units = actionSheetController.textFields![2]
+            
+            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss ZZZ"
+            let hh = (hours.text!).stringByReplacingOccurrencesOfString("HH ",withString: "")
+            let mm = (minutes.text!).stringByReplacingOccurrencesOfString("MM ",withString: "")
+            let uu = (units.text!).stringByReplacingOccurrencesOfString("UU ",withString: "")
+            print("\(hh):\(mm):\(uu)")
+
+            let str = "2015-12-21 \(hh):\(mm):\(uu) -600"
+            print("str \(str)")
+            self.dueTimeLbl.text = str
+            
+            let someDateTime = formatter.dateFromString(str)
+            print("\(someDateTime)")
+            print("HH: \(hours.text!) MM: \(minutes.text!) UU: \(units.text!)")
+
+        }
+        actionSheetController.addAction(startAction)
+        
+        
+        //Add a text field
+        actionSheetController.addTextFieldWithConfigurationHandler { textField -> Void in
+            //TextField configuration
+            textField.text = "HH \(dateComponents.hour)"
+            textField.textColor = UIColor.blueColor()
+            textField.keyboardType = UIKeyboardType.NumberPad
+        }
+        
+        actionSheetController.addTextFieldWithConfigurationHandler { textField -> Void in
+            //TextField configuration
+            textField.text = "MM \(dateComponents.minute + 1)"
+            textField.textColor = UIColor.blueColor()
+            textField.keyboardType = UIKeyboardType.NumberPad
+        }
+        
+        actionSheetController.addTextFieldWithConfigurationHandler { textField -> Void in
+            //TextField configuration
+            textField.text = "UU 00"
+            textField.textColor = UIColor.blueColor()
+            textField.keyboardType = UIKeyboardType.NumberPad
+        }
+        
+        //Present the AlertController
+        self.presentViewController(actionSheetController, animated: true, completion: nil)
+    }
+
 
     @IBAction func carNumberBtn(sender: AnyObject) {
     }
     
-    @IBAction func addTimeBtn(sender: AnyObject) {
-    }
-    
+
     func updateTimeLabel() {
         
         catNumberLbl.text = String(carNumber)
@@ -49,7 +120,7 @@ class ViewController: UIViewController {
         let calendar = NSCalendar.currentCalendar()
         let dateComponents = calendar.components([NSCalendarUnit.Day, NSCalendarUnit.Month, NSCalendarUnit.Year, NSCalendarUnit.WeekOfYear, NSCalendarUnit.Hour, NSCalendarUnit.Minute, NSCalendarUnit.Second, NSCalendarUnit.Nanosecond], fromDate: currentDate)
         
-        print("second = \(dateComponents.second)")
+//        print("second = \(dateComponents.second)")
         
         
         let unit = Double(dateComponents.second)
